a9c4a5ae978662ebfe8c823410bca59d20999cb1
Bhuvaneswari-test <puvineelu@gmail.com>
2025-07-19 17:14:14 +0530

    Add files via upload
    Understand data types and variables.

---
+++ Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float version = 91.0f;
+		System.out.println(version);
+		String developer = "Google";
+		System.out.println(developer);
+		boolean isBeta = false;
+		System.out.println(isBeta);
+		int releaseyear = 2008;
+		System.out.println(releaseyear);
+		char shortcutkey = 'C';
+		System.out.println(shortcutkey);
+
+	}
+
+}
